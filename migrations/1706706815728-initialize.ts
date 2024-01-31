1f3328c6d6e1596ad89243c70c8d6073f75c3db2
emir <emrhncalskn@gmail.com>
2024-01-31 16:28:34 +0300

    passauth eklendi

---
+++ migrations/1706706815728-initialize.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Initialize1706706815728 implements MigrationInterface {
+    name = 'Initialize1706706815728'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`first_name\` varchar(255) NOT NULL, \`last_name\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`role_id\` int NOT NULL, \`status\` int NOT NULL, \`image\` varchar(255) NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`user\``);
+    }
+
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
+import { PassAuth } from './auth/guards/pass-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
+  @ApiBearerAuth()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  @PassAuth()
   @Get('test')
   async randomDigits(@Res() res: Response) {
     return await this.appService.randomDigits(res);
