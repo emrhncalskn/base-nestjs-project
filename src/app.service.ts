f9f476722ff6fc5994f75cd6d1f65b0a83e54f8e
emir <emrhncalskn@gmail.com>
2024-01-31 16:07:33 +0300

    randomdigits added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async randomDigits(res: Response) {
+    return await res.send(Math.floor(100000 + Math.random() * 900000));
   }
 }
